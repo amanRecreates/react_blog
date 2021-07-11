ed5ecf495a099ed28dc3c61f36e5dff509de38db
amanRecreates <amanofficial950@gmail.com>
2021-07-11 09:01:49 +0530

    learned about events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello, Ninjas !!')
+    }
+    
     return (
         <div className="home">
             <h2>Homepge</h2>
+            <button onClick={() => handleClick}>Click Me</button>
         </div>
     );
 }
