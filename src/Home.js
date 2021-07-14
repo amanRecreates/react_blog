279b92d85947d7456992368693c3b1529c5021a1
amanRecreates <amanofficial950@gmail.com>
2021-07-14 11:07:51 +0530

    handle fetchErrors

--- src/Home.js
+++ src/Home.js
@@ -11,21 +11,31 @@ const Home = () => {
    }
 
    const [loading, setLoading] = useState(true)
+   const [error, setError] = useState(null)
    
    useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch the data from the specified resource')
+                }
                 return res.json()
             })
             .then(data => {
                 setBlogs(data)
                 setLoading(false)
+                setError(null)
+            })
+            .catch(err => {
+                setLoading(false)
+                setError(err.message)
             })
    }, [])
    
     return (
         <div className="home">
             { loading && <div> Loading ... </div>}
+            { error && <div> {error} </div>}
             { blogs && <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} /> }
         </div>
     );
