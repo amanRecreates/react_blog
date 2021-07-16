bcbc18cf6e70aae0f69b60e77df1a162425cf6a5
amanRecreates <amanofficial950@gmail.com>
2021-07-16 09:26:03 +0530

    useHistory used for redirecting

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
 
@@ -6,7 +7,7 @@ const Create = () => {
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('yoshi')
     const [loading, setLoading] = useState(false)
-    
+    const history = useHistory()
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -21,6 +22,7 @@ const Create = () => {
         }).then(() => {
             console.log('new blog added')
             setLoading(false)
+            history.push('/')
         })
     }
 
