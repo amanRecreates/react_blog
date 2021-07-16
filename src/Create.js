6b942fd7e1979474dd4b9cda1feaebac311b64b5
amanRecreates <amanofficial950@gmail.com>
2021-07-16 09:14:05 +0530

    added loading state in button while POST request

--- src/Create.js
+++ src/Create.js
@@ -5,19 +5,22 @@ const Create = () => {
     const [title, setTitle] = useState('')
     const [body, setBody] = useState('')
     const [author, setAuthor] = useState('yoshi')
-    const [loading, setLoading] = useState('false')
+    const [loading, setLoading] = useState(false)
     
 
     const handleSubmit = (e) => {
         e.preventDefault()
         const blog = { title, body, author }
         
+        setLoading(true)
+
         fetch('http://localhost:8000/blogs', {
             method: 'POST',
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(blog)
         }).then(() => {
             console.log('new blog added')
+            setLoading(false)
         })
     }
 
@@ -47,7 +50,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                { loading && <button disabled>Adding Blog...</button> }
+                { !loading && <button>Add Blog</button> }
             </form>
         </div>
     );
